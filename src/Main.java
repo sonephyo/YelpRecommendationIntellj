e8ec8b7b27bafde804e383dd3d83d018692114ed
hsusan <hsuthawdar123@gmail.com>
2024-02-18 15:51:51 -0500

    added new code

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Hashtable;
-import java.util.List;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -42,17 +42,100 @@ public class Main {
 
         String lineReview;
         int reviewcount = 0;
-        List<Review> reviewList = new ArrayList<>();
-        while ((lineReview = brReview.readLine()) != null && reviewcount <=1000) {
+        int reviewLengthToParse = 10000;
+
+        Review[] reviewList = new Review[reviewLengthToParse];
+
+        while ((lineReview = brReview.readLine()) != null && reviewcount < reviewLengthToParse) {
             Review r1 = gsonReview.fromJson(lineReview, Review.class);
-            reviewList.add(r1);
-            //System.out.println(r1);
+            reviewList[reviewcount] = r1;
             reviewcount++;
+        }
+
+        long startTime = System.nanoTime();
+
+        String userInput = "asian food";
+        String[] inputSplit = cleanString(userInput);
+//        System.out.println(Arrays.toString(inputSplit));
+
+
+        int[] dfCount = new int[inputSplit.length];
+        for(Review review: reviewList) {
+            review.init_FreqTableForEachReview(inputSplit);
+            String[] cleanReviewData = cleanString(review.getReview_text());
+//            System.out.println(Arrays.toString(cleanReviewData));
+
+
+            for(String i: cleanReviewData) {
+                for(int j = 0; j < inputSplit.length; j++) {
+                    if (inputSplit[j].equalsIgnoreCase(i)){
+                        review.incrementCountOfEach(j);
+                        review.setTrueContainsWord(j);
+                    }
+                }
+            }
+            for(int i = 0; i < review.getContainsWord().length; i++) {
+                if(review.getContainsWord()[i]) {
+                    dfCount[i]++;
+                }
+            }
+
+//            System.out.println(Arrays.toString(review.getContainsWord()));
+//            System.out.println(Arrays.toString(review.getCountOfEachWord()));
+        }
+//        System.out.println(Arrays.toString(dfCount));
+
+        for(Review r:reviewList) {
+            r.setTotalWeight(calculateWeight(r.getCountOfEachWord(),dfCount,reviewLengthToParse));
+//            System.out.println(r.getTotalWeight());
+        }
+
+        Arrays.sort(reviewList, new Comparator<Review>() {
+            @Override
+            public int compare(Review r1, Review r2) {
+                return Double.compare(r2.getTotalWeight(), r1.getTotalWeight());
+            }
+        });
+
+        long endTime = System.nanoTime();
+
+        long totalTime = endTime - startTime;
+        double milliseconds = totalTime / 1e6;
+        System.out.println("--------------Elapsed time: " + milliseconds + " milliseconds");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(reviewList[i].getTotalWeight());
+            System.out.println(reviewList[i]);
+            System.out.println(Arrays.toString(reviewList[i].getContainsWord()));
+            System.out.println(Arrays.toString(reviewList[i].getCountOfEachWord()));
 
         }
-        Gson gsonWrite = new GsonBuilder().setPrettyPrinting().create();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter ("src/database/review1.json"))){
-            gsonWrite.toJson(reviewList, writer);
+
+
+
+
+
+
+
+    }
+
+    private static double calculateWeight(int[] tfData,int[] dfData, int totalReview) {
+        double total = 0;
+        for (int i = 0; i < tfData.length; i++) {
+            total += Math.log10(1+tfData[i])*((double) totalReview /dfData[i]);
         }
+        return total;
+    }
+
+
+
+    private static String[] cleanString(String rawString) {
+        rawString = rawString.replaceAll("[^a-zA-Z]", " ");
+        rawString = rawString.replaceAll("\\b(to|a|the|and|there|in|is|are|for|I|we|on|would|have)\\b", " ");
+        rawString = rawString.toLowerCase(Locale.ROOT);
+
+
+        return Arrays.stream(rawString.split("\\s+")).filter(s -> !s.isEmpty()).toArray(String[]::new);
+
     }
 }
